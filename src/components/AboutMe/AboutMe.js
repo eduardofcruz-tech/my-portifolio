0f6d40266a8ea091dfb66777412f0873ced2b2ec
Eduardo França <eduardofcruz12@gmail.com>
2025-03-08 23:03:39 -0400

    Update AboutMe.js

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -13,7 +13,7 @@ function AboutMe() {
                     <p>
                         Olá, meu nome é Eduardo França e sou um estudante de Engenharia da Computação apaixonado por tecnologia. Desde cedo, tenho curiosidade em entender como as coisas funcionam, o que me levou a explorar diversas áreas da computação. Atualmente, estou focado em aprimorar minhas habilidades em HTML, CSS, JavaScript, React e Python, além de utilizar o GitHub para versionamento de projetos.<br></br><br></br>
                         Meus objetivos profissionais são amplos e abrangem áreas como desenvolvimento backend, banco de dados, segurança da informação e automação. Busco constantemente aprender e me adaptar às novas tecnologias, sempre com o objetivo de contribuir para projetos inovadores.<br></br><br></br>
-                        Nas horas vagas, gosto de me dedicar à leitura, música, filmes e passeios, buscando sempre equilibrar o aprendizado com momentos de lazer.
+                        Nas horas vagas, gosto de me dedicar à leitura, música e filmes, buscando sempre equilibrar o aprendizado com momentos de lazer.
                     </p>
                 </div>
 
