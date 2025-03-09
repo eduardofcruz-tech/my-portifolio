f818da3e8b4bc1a1ff69690a89a3aa7d0e431ace
Eduardo França <eduardofcruz12@gmail.com>
2025-03-08 22:52:57 -0400

    teste alteração

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 function Header() {
     return (
         <header className="header">
-            <h1>EF <spam>&lt;&frasl;&gt;</spam></h1>
+            <h1>EF. <spam>&lt;&frasl;&gt;</spam></h1>
             <nav className="nav nav--main">
                 <ul className="ul ul--main">
                     <li><a className='a a--nav-main' href="#home">Home</a></li>
