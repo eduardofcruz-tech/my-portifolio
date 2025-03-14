b898e9f1a006bfd8648c8ef46a94506382f474f3
eduardofcruz-tech <eduardofcruz12@gmail.com>
2025-03-14 08:43:54 -0400

    logo, texto

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,9 +10,13 @@ function AboutMe() {
                 <div className='div--about-me-section'>
                     <p className='p--about-me-title'>Sobre mim</p>
                     <p className='p--about-me-occupation'>Sou Estudante de<br></br>Engenharia da Computação</p>
-                    <p>
-                        Olá, meu nome é Eduardo França e sou um estudante de Engenharia da Computação apaixonado por tecnologia. Desde cedo, tenho curiosidade em entender como as coisas funcionam, o que me levou a explorar diversas áreas da computação. Atualmente, estou focado em aprimorar minhas habilidades em HTML, CSS, JavaScript, React e Python, além de utilizar o GitHub para versionamento de projetos.<br></br><br></br>
-                        Meus objetivos profissionais são amplos e abrangem áreas como desenvolvimento backend, banco de dados, segurança da informação e automação. Busco constantemente aprender e me adaptar às novas tecnologias, sempre com o objetivo de contribuir para projetos inovadores.<br></br><br></br>
+                    <p className='p--about-me-text'>
+                        Olá, meu nome é Eduardo França e sou um estudante de Engenharia da Computação apaixonado por tecnologia. Desde cedo, tenho curiosidade em entender como as coisas funcionam, o que me levou a explorar diversas áreas da computação. Atualmente, estou focado em aprimorar minhas habilidades em HTML, CSS, JavaScript, React e Python, além de utilizar o GitHub para versionamento de projetos.
+                    </p>
+                    <p className='p--about-me-text'>
+                        Meus objetivos profissionais são amplos e abrangem áreas como desenvolvimento backend, banco de dados, segurança da informação e automação. Busco constantemente aprender e me adaptar às novas tecnologias, sempre com o objetivo de contribuir para projetos inovadores.
+                    </p>
+                    <p className='p--about-me-text'>
                         Nas horas vagas, gosto de me dedicar à leitura, música, filmes e passeios, buscando sempre equilibrar o aprendizado com momentos de lazer.
                     </p>
                 </div>
